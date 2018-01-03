78e67c5a1f31474fe016ff70627d88b0ada8c9d5
Anthematics <jasonharder2@gmail.com>
2018-01-03 16:59:23 -0500

    finished repeat challange just adding my basic solution to Ghub

---
+++ repeat.js
@@ -0,0 +1,11 @@
+function repeatStringNumTimes(str, num) {
+ if (num < 0)
+return ""
+ if (num > 0)
+  return str.repeat(num) 
+}
+
+repeatStringNumTimes("abc", 3);
+
+//quick script that repeats a given string 
+//free code camp challange
